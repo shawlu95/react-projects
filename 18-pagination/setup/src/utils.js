ecc2b4ff7500149200d6ee113694152e6b7837f6
Shaw Lu <shawlu95@gmail.com>
2022-04-09 11:10:32 -0700

    Package pages in util

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,15 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const pageSize = 9;
+  const pageCount = Math.ceil(followers.length / pageSize);
+  console.log({ pageSize, pageCount });
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    const start = index * pageSize;
+    return followers.slice(start, start + pageSize); // ok to pass end index
+  });
+
+  console.log({ pages });
+
+  return pages;
+}
 
 export default paginate
