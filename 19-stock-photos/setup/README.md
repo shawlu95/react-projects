7188e4f2ec4adef2e3ffff1f404f3805e96d2f87
Shaw Lu <shawlu95@gmail.com>
2022-04-09 12:10:59 -0700

    Stock photos initial load

---
+++ 19-stock-photos/setup/README.md
@@ -0,0 +1,5 @@
+* Documentation: https://unsplash.com/documentation
+* end point: `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`
+* `.env` must be placed in root (with `package.json`), not in `src`
+* every time a new env var is added/updated, server must be restarted to pick up the change.
+* no `dotenv` package required
--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    let url;
+    url = `${mainUrl}${clientID}`;
+    try {
+      const resposne = await fetch(url);
+      const data = await resposne.json();
+      setPhotos(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.response);
+      setLoading(false);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  return <main>
+    <section className="search">
+      <form action="" className="search-form">
+        <input type="text" placeholder='search' className="form-input" />
+        <button className="submit-btn" type='submit' onClick={handleSubmit}>
+          <FaSearch />
+        </button>
+      </form>
+    </section>
+    <section className="photos">
+      <div className="photos-center">
+        {photos.map((image, index) => {
+          return <Photo key={image.id} {...image} />
+        })}
+      </div>
+      {loading && <h2 className='loading'>Loading...</h2>}
+    </section>
+  </main>
 }
 
 export default App
