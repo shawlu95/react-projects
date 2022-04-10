cb12e8c4b62c6a084a14a54a697272e522b5c109
Shaw Lu <shawlu95@gmail.com>
2022-04-09 22:29:56 -0700

    Movie single page

--- 21-movie-db/setup/src/SingleMovie.js
+++ 21-movie-db/setup/src/SingleMovie.js
@@ -3,7 +3,51 @@ import { useParams, Link } from 'react-router-dom'
 import { API_ENDPOINT } from './context'
 
 const SingleMovie = () => {
-  return <h2>single movie</h2>
+  const { id } = useParams();
+  console.log("movie id", id);
+  const [movie, setMovie] = useState({});
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  const fetchMovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.response === 'False') {
+      setError({ show: true, msg: data.Error });
+      setLoading(false);
+    } else {
+      setMovie(data);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovie(`${API_ENDPOINT}&i=${id}`);
+  }, [id]);
+
+  if (isLoading) {
+    return <div className="loading"></div>
+  }
+
+  if (error.show) {
+    return <div className="page-error">
+      <h1>{error.msg}</h1>
+      <Link to='/' className="btn">
+        back to movies
+      </Link>
+    </div>
+  }
+
+  const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
+  return <section className="single-movie">
+    <img src={poster} alt={title} />
+    <div className="single-movie-info">
+      <h2>{title}</h2>
+      <p>{plot}</p>
+      <h4>{year}</h4>
+      <Link className="btn" to="/">Back to Movies</Link>
+    </div>
+  </section>
 }
 
 export default SingleMovie
