9a6006da4d6435f602b45084fca89189b473b930
Shaw Lu <shawlu95@gmail.com>
2022-04-09 21:29:04 -0700

    Dark theme toggle

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -3,11 +3,24 @@ import data from './data'
 import Article from './Article'
 
 function App() {
+  const [theme, setTheme] = useState('light-theme');
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
   return <main>
     <nav>
       <div className="nav-center">
         <h1>Overreacted</h1>
-        <button className="btn">Toggle</button>
+        <button className="btn" onClick={toggleTheme}>Toggle</button>
       </div>
     </nav>
     <section className="articles">
