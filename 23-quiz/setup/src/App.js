b2359da59285c0d47d6d26ae9f312bc980db5255
Shaw Lu <shawlu95@gmail.com>
2022-04-10 11:43:45 -0700

    Quic display question

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -5,7 +5,36 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  return <h2>quiz starter</h2>
+  const {
+    waiting, loading, questions, index, correct, nextQuestion } = useGlobalContext();
+
+  if (waiting) {
+    return <SetupForm />
+  }
+
+  if (loading) {
+    return <Loading />
+  }
+  console.log(questions[0])
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+  return <main>
+    {/* <Modal /> */}
+    <section className="quiz">
+      <p className="correct-answers">
+        correct answer: {correct} / {index}
+      </p>
+      <article className="container">
+        <h2 dangerouslySetInnerHTML={{ __html: question }} />
+        <div className='btn-container'>
+          {answers.map((answer, index) => {
+            return <button key={index} className='answer-btn' dangerouslySetInnerHTML={{ __html: answer }}></button>
+          })}
+        </div>
+      </article>
+      <button className="next-question" onClick={nextQuestion}>Next Question</button>
+    </section>
+  </main>
 }
 
 export default App
--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -10,11 +10,65 @@ const table = {
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
-
+const tempUrl = 'https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  // display form when waiting
+  const [waiting, setWaiting] = useState(true);
+
+  // fetching data
+  const [loading, setLoading] = useState(true);
+
+  const [questions, setQuestions] = useState([]);
+
+  // current question
+  const [index, setIndex] = useState(0);
+
+  // number of correct answers
+  const [correct, setCorrect] = useState(0);
+
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch(err => console.log(err));
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      }
+    } else {
+      setWaiting(true);
+      setError(true);
+    }
+    console.log(response);
+  };
+
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1;
+      if (index > questions.length - 1) {
+        // openModal();
+        return 0;
+      }
+      return index;
+    })
+  }
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
+
+  return <AppContext.Provider value={{
+    waiting, loading, questions, index, correct, error, isModalOpen, nextQuestion
+  }}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
