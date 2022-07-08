f4e6478a50dd3e3b9e34926f29708a42ceffa644
Shaw Lu <shawlu95@gmail.com>
2022-07-07 22:13:21 -0700

    Nit: remove placeholder text

--- 13-stripe-submenus/setup/src/App.js
+++ 13-stripe-submenus/setup/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Hero from './Hero'
-import Sidebar from './Sidebar'
-import Submenu from './Submenu'
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
 function App() {
   return (
     <>
@@ -10,9 +10,8 @@ function App() {
       <Sidebar></Sidebar>
       <Hero></Hero>
       <Submenu></Submenu>
-      <h2>stripe submenus setup</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
