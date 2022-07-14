83deb727da27cde170eaa065a2d211d6339576f9
Shaw Lu <shawlu95@gmail.com>
2022-07-14 00:00:30 -0700

    Pass in payload to dispatch, access "ownProps" in mapDispatchToProps

--- 14-cart/setup/src/CartItem.js
+++ 14-cart/setup/src/CartItem.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { useGlobalContext } from './context';
-const CartItem = ({ id, img, title, price, amount }) => {
-  const { remove, increase, decrease, toggleAmount } = useGlobalContext();
+import { connect } from 'react-redux';
+import { REMOVE, INCREASE, DECREASE, TOGGLE_AMOUNT } from './actions';
+
+const CartItem = ({
+  id,
+  img,
+  title,
+  price,
+  amount,
+  remove,
+  increase,
+  decrease,
+  toggleAmount,
+}) => {
   return (
     <article className='cart-item'>
       <img src={img} alt={title} />
@@ -15,7 +26,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
       </div>
       <div>
         {/* increase amount */}
-        <button className='amount-btn' onClick={() => toggleAmount(id, 'inc')}>
+        <button className='amount-btn' onClick={() => increase()}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
           </svg>
@@ -23,7 +34,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
         {/* amount */}
         <p className='amount'>{amount}</p>
         {/* decrease amount */}
-        <button className='amount-btn' onClick={() => toggleAmount(id, 'dec')}>
+        <button className='amount-btn' onClick={() => decrease()}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
           </svg>
@@ -33,4 +44,14 @@ const CartItem = ({ id, img, title, price, amount }) => {
   );
 };
 
-export default CartItem;
+// already getting bunch of props from CartContainer, no need to query state
+// only pass down dispath function, pass null to first arg
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    remove: (id) => dispatch({ type: REMOVE, payload: id }),
+    increase: () => dispatch({ type: INCREASE, payload: ownProps.id }),
+    decrease: () => dispatch({ type: DECREASE, payload: ownProps.id }),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(CartItem);
