cbb71f33660b8b28455f3d8058a645de463611f0
Shaw Lu <shawlu95@gmail.com>
2022-07-13 23:55:08 -0700

    Use switch statement in reducer

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -11,67 +11,72 @@ import {
 
 const reducer = (state, action) => {
   console.log(action);
-  if (action.type === CLEAR_CART) {
-    return { ...state, cart: [] };
-  } else if (action.type === REMOVE) {
-    return {
-      ...state,
-      cart: state.cart.filter((item) => item.id !== action.payload),
-    };
-  } else if (action.type === INCREASE) {
-    let cart = state.cart.map((item) => {
-      if (item.id === action.payload) {
-        return { ...item, amount: item.amount + 1 };
-      }
-      return item;
-    });
-    return { ...state, cart: cart };
-  } else if (action.type === DECREASE) {
-    let cart = state.cart
-      .map((item) => {
+
+  switch (action.type) {
+    case CLEAR_CART:
+      return { ...state, cart: [] };
+    case REMOVE:
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
+    case INCREASE: {
+      let cart = state.cart.map((item) => {
         if (item.id === action.payload) {
-          // remove item if amount drops to zero
-          return { ...item, amount: item.amount - 1 };
+          return { ...item, amount: item.amount + 1 };
         }
         return item;
-      })
-      .filter((item) => item.amount !== 0);
-    return { ...state, cart: cart };
-  } else if (action.type === GET_TOTAL) {
-    let { total, amount } = state.cart.reduce(
-      (cartTotal, cartItem) => {
-        const { price, amount } = cartItem;
-        cartTotal.total += amount * price;
-        cartTotal.amount += amount;
-        return cartTotal;
-      },
-      { total: 0, amount: 0 }
-    );
-    total = parseFloat(total.toFixed(2));
-    return { ...state, total, amount };
-  } else if (action.type === LOADING) {
-    return { ...state, loading: true };
-  } else if (action.type === DISPLAY_ITEMS) {
-    return { ...state, cart: action.payload, loading: false };
-  } else if (action.type === TOGGLE_AMOUNT) {
-    const { id, type } = action.payload;
-
-    let cart = state.cart
-      .map((item) => {
-        if (item.id === id) {
-          if (type === 'inc') {
-            return { ...item, amount: item.amount + 1 };
-          } else if (type === 'dec') {
+      });
+    }
+    case DECREASE: {
+      let cart = state.cart
+        .map((item) => {
+          if (item.id === action.payload) {
+            // remove item if amount drops to zero
             return { ...item, amount: item.amount - 1 };
           }
-        }
-        return item;
-      })
-      .filter((item) => item.amount !== 0);
-    return { ...state, cart };
+          return item;
+        })
+        .filter((item) => item.amount !== 0);
+      return { ...state, cart: cart };
+    }
+    case GET_TOTAL: {
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          cartTotal.total += amount * price;
+          cartTotal.amount += amount;
+          return cartTotal;
+        },
+        { total: 0, amount: 0 }
+      );
+      total = parseFloat(total.toFixed(2));
+      return { ...state, total, amount };
+    }
+    case LOADING:
+      return { ...state, loading: true };
+    case DISPLAY_ITEMS:
+      return { ...state, cart: action.payload, loading: false };
+    case TOGGLE_AMOUNT: {
+      const { id, type } = action.payload;
+
+      let cart = state.cart
+        .map((item) => {
+          if (item.id === id) {
+            if (type === 'inc') {
+              return { ...item, amount: item.amount + 1 };
+            } else if (type === 'dec') {
+              return { ...item, amount: item.amount - 1 };
+            }
+          }
+          return item;
+        })
+        .filter((item) => item.amount !== 0);
+      return { ...state, cart };
+    }
+    default:
+      return state;
   }
-  // return old state if no matching action found
-  return state;
 };
 
 export default reducer;
